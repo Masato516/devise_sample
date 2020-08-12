8a87502983dbd48b6bcb9e21ef817fc068306b24
Masato516 <yaginen9@gmail.com>
2020-08-12 14:14:22 +0900

    first commit

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  # before_action :authenticate_user!
+  # Include default devise modules. Others available are:
+  #  :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :confirmable,
+         :timeoutable, :trackable
+end
---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+  resources :boards
+  #ログイン画面
+  root to: 'boards#index'
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations',
+    passwords: 'users/passwords',
+    confirmations: 'users/confirmations'
+  }
+  # devise_scope :user do
+  #   get "sign_in", :to => "users/sessions#new"
+  #   get "sign_out", :to => "users/sessions#destroy"
+  # end
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
